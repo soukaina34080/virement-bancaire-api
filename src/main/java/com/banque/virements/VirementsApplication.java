89d659c4d7e5f556e942c7be8501dfa26b326dc3
MESSAOUDI Soukaina <soukaina.messaoudi@infotel.com>
2026-06-08 17:00:55 +0200

    chore: init projet Spring Boot - structure DDD

---
+++ src/main/java/com/banque/virements/VirementsApplication.java
@@ -0,0 +1,13 @@
+package com.banque.virements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banque/virements/VirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banque.virements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
