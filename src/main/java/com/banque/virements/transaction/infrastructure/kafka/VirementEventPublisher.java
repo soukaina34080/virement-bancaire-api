98650a69f99603e22b89dd5e8f39a9dd8d715eef
s_mess <soukaina.messaoudi02@gmail.com>
2026-06-24 10:33:42 +0200

    kafka consumer

--- src/main/java/com/banque/virements/transaction/infrastructure/kafka/VirementEventPublisher.java
+++ src/main/java/com/banque/virements/transaction/infrastructure/kafka/VirementEventPublisher.java
@@ -1,18 +1,17 @@
 package com.banque.virements.transaction.infrastructure.kafka;
 
 import com.banque.virements.transaction.domain.Virement;
-import com.banque.virements.transaction.domain.VirementEventPublisher;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class VirementEventPublisherKafka implements VirementEventPublisher {
+public class VirementEventPublisher implements com.banque.virements.transaction.domain.VirementEventPublisher {
 
     private static final String TOPIC = "virements-crees";
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public VirementEventPublisherKafka(KafkaTemplate<String, String> kafkaTemplate) {
+    public VirementEventPublisher(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
