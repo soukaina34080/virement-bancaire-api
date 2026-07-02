5901d9471dc3d8d2736625665850ac01e8b3ecfc
soukaina34080 <45172668+soukaina34080@users.noreply.github.com>
2026-07-02 08:36:52 +0000

    elastic search settings

--- src/main/java/com/banque/virements/transaction/infrastructure/elasticsearch/VirementDocument.java
+++ src/main/java/com/banque/virements/transaction/infrastructure/elasticsearch/VirementDocument.java
@@ -1,30 +1,29 @@
-//package com.banque.virements.transaction.infrastructure.elasticsearch;
-//
-//import lombok.Data;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.elasticsearch.annotations.Document;
-//import org.springframework.data.elasticsearch.annotations.Field;
-//import org.springframework.data.elasticsearch.annotations.FieldType;
-//
-//import java.math.BigDecimal;
-//
-//@Data
-//@Document(indexName = "virements")
-//public class VirementDocument {
-//
-//    @Id
-//    private String id;
-//
-//    @Field(type = FieldType.Keyword)
-//    private String ibanEmetteur;
-//
-//    @Field(type = FieldType.Keyword)
-//    private String ibanBeneficiaire;
-//
-//    @Field(type = FieldType.Double)
-//    private BigDecimal montant;
-//
-//    @Field(type = FieldType.Keyword)
-//    private String statut;
-//
-//}
+package com.banque.virements.transaction.infrastructure.elasticsearch;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.math.BigDecimal;
+
+@Data
+@Document(indexName = "virements")
+public class VirementDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Keyword)
+    private String ibanEmetteur;
+
+    @Field(type = FieldType.Keyword)
+    private String ibanBeneficiaire;
+
+    @Field(type = FieldType.Double)
+    private BigDecimal montant;
+
+    @Field(type = FieldType.Keyword)
+    private String statut;
+}
